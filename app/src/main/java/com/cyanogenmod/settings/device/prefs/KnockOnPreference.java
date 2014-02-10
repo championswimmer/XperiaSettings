91cc011a1a37e8c79618eff1f8d66dbdfa5b9081
Arnav Gupta <championswimmer@aokp.co>
2014-02-10 23:27:27 +0530

    add KnockOn preference

---
+++ app/src/main/java/com/cyanogenmod/settings/device/prefs/KnockOnPreference.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2014 Arnav Gupta, AOKP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device.prefs;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.cyanogenmod.settings.device.R;
+import com.cyanogenmod.settings.device.Utils;
+
+/**
+ * Created by championswimmer on 10/2/14.
+ */
+public class KnockOnPreference extends CheckBoxPreference implements CheckBoxPreference.OnPreferenceChangeListener {
+
+    public static String TAG = "PenModePreference";
+
+    public static String SYSFS_PATH = null;
+    public static String ENABLED_VALUE;
+    public static String DISABLED_VALUE;
+    public static Boolean SUPPORTED;
+
+    private Context CONTEXT;
+
+    public KnockOnPreference(final Context context, final AttributeSet attrst) {
+        super(context, attrst);
+        CONTEXT = context;
+        SYSFS_PATH = context.getString(R.string.knock_on_sysfs_file);
+        ENABLED_VALUE = context.getString(R.string.knock_on_enabled_value);
+        DISABLED_VALUE = context.getString(R.string.knock_on_enabled_value);
+        SUPPORTED = context.getResources().getBoolean(R.bool.has_knock_on);
+    }
+
+    @Override
+    protected void onBindView(final View v) {
+        super.onBindView(v);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object o) {
+        Boolean state = o.toString().equalsIgnoreCase("true");
+        String val = getValueFromState(state);
+        Utils.writeValue(SYSFS_PATH, val);
+        return true;
+
+    }
+
+    public static void restore(Context context) {
+        SYSFS_PATH = context.getString(R.string.knock_on_sysfs_file);
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+        String value = settings.getString("knock_on", DISABLED_VALUE);
+        Utils.writeValue(SYSFS_PATH, value);
+    }
+
+    public String getValueFromState(Boolean state) {
+        if (state) {
+            return ENABLED_VALUE;
+        } else {
+            return DISABLED_VALUE;
+        }
+    }
+
+    public Boolean checkSupport() {
+        Boolean fileExists = Utils.fileExists(SYSFS_PATH);
+        //Log.d(TAG, "File exists : " + fileExists);
+        //Log.d(TAG, "Enabled via config : " + isEnabledInConfig);
+        if ((SUPPORTED && fileExists)) {
+            return true;
+        } else {
+            setSummary(R.string.summary_unsupported);
+            setEnabled(false);
+            return false;
+        }
+    }
+
+}
